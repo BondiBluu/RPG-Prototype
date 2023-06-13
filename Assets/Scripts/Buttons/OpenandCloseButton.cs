3700bc41b50a1c30bc662d36e0ea5e100d9169e6
BamideleOgundeji <bogundeji15@saintpeters.edu>
2023-06-13 12:55:52 -0400

    creation of button

---
+++ Assets/Scripts/Buttons/OpenandCloseButton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenandCloseButton : MonoBehaviour
+{
+    [SerializeField] GameObject openButton;
+    [SerializeField] GameObject closeButton;
+
+    public void OpenButton()
+    {
+
+    }
+
+    public void CloseButton()
+    {
+
+    }
+}
