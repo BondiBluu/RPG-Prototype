9da9e78b740ad4fe4321cc2e942f00d21f02b079
BamideleOgundeji <bogundeji15@saintpeters.edu>
2023-07-31 16:27:01 -0400

    Buttons created for showing attacking and supplementary moves

--- Assets/Scripts/For Battle/AttackandSupplementary.cs
+++ Assets/Scripts/For Battle/AttackandSupplementary.cs
@@ -1,43 +1,48 @@
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public class AttackandSupplementary : MonoBehaviour
 {
     //make a list for the moves
     [SerializeField] List<Button> atkButtons;
-    [SerializeField] List<Button> suppButtons;
     [SerializeField] GameObject atkUI;
     [SerializeField] GameObject suppUI;
     [SerializeField] TMP_Text atkText;
-    [SerializeField] TMP_Text suppText;
-    public bool attackIsUp;
-    // Start is called before the first frame update
-    void Start()
+    public bool actionIsUp = false;
+    MoveGenerator moveGenerator;
+
+    private void Start()
     {
-        attackIsUp = false;
+        moveGenerator = FindObjectOfType<MoveGenerator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    //for the attack button
     public void OnATKButton()
     {
         atkUI.SetActive(true);
-        attackIsUp = true;
+        suppUI.SetActive(false);
+        actionIsUp = true;
+        moveGenerator.GenerateATKButtons();
+    }
+    
+    //for te support button
+    public void OnSUPPButton()
+    {
+        suppUI.SetActive(true);
+        atkUI.SetActive(false);
+        actionIsUp = true;
+        moveGenerator.GenerateSUPPButtons();
     }
 
     public void OnBackButton()
     {
-        if (attackIsUp == true)
+        if (actionIsUp == true)
         {
+            suppUI.SetActive (false);
             atkUI.SetActive(false);
-            attackIsUp = false;
+            actionIsUp = false;
         }
     }
 
