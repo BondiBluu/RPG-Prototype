f7a51de5fa6099075c13be6f5bca50b6c99f20df
BamideleOgundeji <bogundeji15@saintpeters.edu>
2023-08-01 13:38:48 -0400

    added onClick event

--- Assets/Scriptable Objects/Moves/MoveGenerator.cs
+++ Assets/Scriptable Objects/Moves/MoveGenerator.cs
@@ -50,11 +50,15 @@ public class MoveGenerator : MonoBehaviour
                     Debug.Log("Moves available.");
                     Debug.Log(move.AttackName);
                     Debug.Log("Level gotten: " + move.LevelAqcuired);
-                    //instantiating a button component onto the prefab, this way we won't need a button in the hierarchy
-                    //Button button = buttonGO.AddComponent<Button>();
+                    
                     buttonGO.GetComponentInChildren<TMP_Text>().text = move.AttackName;
                     //adding the move to the list
                     movesAlreadyAdded.Add(move);
+
+                    //prefabs can't have onClick events, so we'll have to get the component and add an onClick event.
+                    Button buttonComponent = buttonGO.GetComponent<Button>();
+                    buttonComponent.onClick.AddListener(() => OnButtonClick());
+
                 }
             }
         }
@@ -62,34 +66,37 @@ public class MoveGenerator : MonoBehaviour
     
     public void GenerateATKButtons()
     {
-        //spacing out the buttons
         float currentPosY = 0f;
 
-        //if the player's level is more or equal to the required amount, show the move. Needs to be changed to add buttons when the required level is reached
         if (characterStatistics != null)
         {
             foreach (MoveBaseClass move in characterStatistics.moveBaseClassList)
             {
-                //if the move's level is more than or equal to out chara's level
                 if (characterStatistics.level >= move.LevelAqcuired && !movesAlreadyAdded.Contains(move) && move.MoveType == MoveType.DAMAGING)
                     {
-                    //making a new move button- instantiating the prefab and having it be a child of suppButtonContainer
                     GameObject buttonGO = Instantiate(moveButtonPrefab, atkButtonContainer);
-                    //setting the anchored pos of the button's rectTransform using a vector 2 
+
                     buttonGO.GetComponent<RectTransform>().anchoredPosition = new Vector2(0f, -currentPosY);
-                    //adding button spacing- next button will appear under the one that was just made
+
                     currentPosY += buttonSpacing + buttonGO.GetComponent<RectTransform>().sizeDelta.y;
                     Debug.Log("Moves available.");
                     Debug.Log(move.AttackName);
                     Debug.Log("Level gotten: " + move.LevelAqcuired);
-                    //instantiating a button component onto the prefab, this way we won't need a button in the hierarchy
-                    //Button button = buttonGO.AddComponent<Button>();
+
                     buttonGO.GetComponentInChildren<TMP_Text>().text = move.AttackName;
-                    //adding the move to the list
+
                     movesAlreadyAdded.Add(move);
+
+                    Button buttonComponent = buttonGO.GetComponent<Button>();
+                    buttonComponent.onClick.AddListener(() => OnButtonClick());
                 }
             }
         }
+    }
+
+    public void OnButtonClick()
+    {
+        Debug.Log("Button clicked");
     }
 
     //pass the selected character's CharacterStatistics data to the MoveGenerator script so it can display the moves for that specific character
@@ -97,4 +104,5 @@ public class MoveGenerator : MonoBehaviour
     {
         characterStatistics = characterData;
     }
+
 }
