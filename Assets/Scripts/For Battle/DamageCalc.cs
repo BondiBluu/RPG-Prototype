72941334dcb24f215479d5c7aad94d5c90e4d822
BamideleOgundeji <bogundeji15@saintpeters.edu>
2023-08-24 15:54:22 -0400

    code for saving moves and calculating damage moved here

--- Assets/Scripts/For Battle/DamageCalc.cs
+++ Assets/Scripts/For Battle/DamageCalc.cs
@@ -1,20 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DamageCalc : MonoBehaviour
 {
-    //for every damage calc in the game
-
-    // Start is called before the first frame update
-    void Start()
+    public int CalcDamage(Unit attacker, MoveBaseClass move, EnemyUnit theAttacked)
     {
-        
+        float damageOutput = (attacker.characterStats.attack * move.AttackPower) / (2 * theAttacked.enemyStats.defense);
+        int finalResult = (int)Math.Ceiling(damageOutput);
+        return finalResult;
+    }
+    public int EnemyCalc(EnemyUnit attacker, MoveBaseClass move, Unit theAttacked)
+    {
+        float damageOutput = (attacker.enemyStats.attack * move.AttackPower) / (2 * theAttacked.characterStats.defense);
+        int finalResult = (int)Math.Ceiling(damageOutput);
+        if(finalResult <= 0)
+        {
+            if(move.MoveType != MoveType.SUPPLEMENTARY)
+            {
+                finalResult = 1;
+            }
+        }
+        return finalResult;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
--- Assets/Scripts/For Battle/PlayerandEnemyActions.cs
+++ Assets/Scripts/For Battle/PlayerandEnemyActions.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class PlayerandEnemyActions : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public List<PlayerActions> playerActionContainer = new List<PlayerActions>();
+    public List<EnemyActions> enemyActionContainer = new List<EnemyActions>();
+
+    public void SavePlayerAction(Unit _playerUnit, MoveBaseClass _move, EnemyUnit _enemyUnit)
     {
-        
+        //adding the player's actions
+        playerActionContainer.Add(new PlayerActions(_playerUnit, _move, _enemyUnit));
+        Debug.Log(playerActionContainer.Count);
+        Debug.Log(_playerUnit.characterStats.characterName + " " + _move.AttackName + ", on " + _enemyUnit.enemyStats.enemyName);
+    }
+    public void SaveEnemyAction(EnemyUnit _enemyUnit, MoveBaseClass _move, Unit _playerUnit)
+    {
+        //adding the player's actions
+        enemyActionContainer.Add(new EnemyActions(_enemyUnit, _move, _playerUnit));
+        Debug.Log(enemyActionContainer.Count);
+        Debug.Log(_enemyUnit.enemyStats.enemyName + " " + _move.AttackName + ", on " + _playerUnit.characterStats.characterName);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    //creating a class to store each player's actions
+    public class PlayerActions
     {
-        
+        public MoveBaseClass move;
+        public Unit playerUnit;
+        public EnemyUnit enemyTarget;
+
+        public PlayerActions(Unit _playerUnit, MoveBaseClass _move, EnemyUnit _enemyUnit)
+        {
+            playerUnit = _playerUnit;
+            move = _move;
+            enemyTarget = _enemyUnit;
+        }
+
+    }
+    public class EnemyActions
+    {
+        public MoveBaseClass move;
+        public EnemyUnit enemyUnit;
+        public Unit playerTarget;
+
+
+        public EnemyActions(EnemyUnit _enemyUnit, MoveBaseClass _move, Unit _playerUnit)
+        {
+            enemyUnit = _enemyUnit;
+            move = _move;
+            playerTarget = _playerUnit;
+        }
     }
 }
