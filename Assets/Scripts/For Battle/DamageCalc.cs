9ae0463b6b4bf163e4a09ca0a370411de8d55c50
BamideleOgundeji <bogundeji15@saintpeters.edu>
2023-08-24 17:22:14 -0400

    added physical and magical damage,

--- Assets/Scripts/For Battle/DamageCalc.cs
+++ Assets/Scripts/For Battle/DamageCalc.cs
@@ -7,19 +7,83 @@ public class DamageCalc : MonoBehaviour
 {
     public int CalcDamage(Unit attacker, MoveBaseClass move, EnemyUnit theAttacked)
     {
-        float damageOutput = (attacker.characterStats.attack * move.AttackPower) / (2 * theAttacked.enemyStats.defense);
-        int finalResult = (int)Math.Ceiling(damageOutput);
+        float damageOutput = 0f;
+        int finalResult = 0;
+
+        if (move.AttackType == AttackType.PHYSICAL)
+        {
+            damageOutput = (attacker.characterStats.attack * move.AttackPower) / (2 * theAttacked.enemyStats.defense);
+            finalResult = (int)Math.Ceiling(damageOutput);
+        }
+        
+        if (move.AttackType == AttackType.MAGICAL)
+        {
+            damageOutput = (attacker.characterStats.magic * move.AttackPower) / (2 * theAttacked.enemyStats.resistance);
+            finalResult = (int)Math.Ceiling(damageOutput);
+        }
+
+        if(move.BoostAmount > 0)
+        {
+            //boost by that amount
+            if(move.SupplementaryEffect == SupplementaryEffect.POSITIVE)
+            {
+                //boost
+                //atk+(atk * .1(the boost amount))
+                //target will be self
+            }
+
+            if (move.SupplementaryEffect == SupplementaryEffect.NEGATIVE)
+            {
+                //lower
+            }
+        }
+        
+        if (finalResult <= 0)
+        {
+            if (move.MoveType != MoveType.SUPPLEMENTARY)
+            {
+                finalResult += 1;
+            }
+        }
         return finalResult;
+
     }
     public int EnemyCalc(EnemyUnit attacker, MoveBaseClass move, Unit theAttacked)
     {
-        float damageOutput = (attacker.enemyStats.attack * move.AttackPower) / (2 * theAttacked.characterStats.defense);
-        int finalResult = (int)Math.Ceiling(damageOutput);
-        if(finalResult <= 0)
+        float damageOutput = 0f;
+        int finalResult = 0;
+
+        if (move.AttackType == AttackType.PHYSICAL)
+        {
+            damageOutput = (attacker.enemyStats.attack * move.AttackPower) / (2 * theAttacked.characterStats.defense);
+            finalResult = (int)Math.Ceiling(damageOutput);
+        }
+
+        if (move.AttackType == AttackType.PHYSICAL)
+        {
+            damageOutput = (attacker.enemyStats.magic * move.AttackPower) / (2 * theAttacked.characterStats.resistance);
+            finalResult = (int)Math.Ceiling(damageOutput);
+        }
+
+        if (move.BoostAmount > 0)
+        {
+            //boost by that amount
+            if (move.SupplementaryEffect == SupplementaryEffect.POSITIVE)
+            {
+                //boost
+            }
+
+            if (move.SupplementaryEffect == SupplementaryEffect.NEGATIVE)
+            {
+                //lower
+            }
+        }
+
+        if (finalResult <= 0)
         {
             if(move.MoveType != MoveType.SUPPLEMENTARY)
             {
-                finalResult = 1;
+                finalResult += 1;
             }
         }
         return finalResult;
