375fbab721f4fd1ceadc708c7174b9933e7ca163
BamideleOgundeji <bogundeji15@saintpeters.edu>
2023-09-01 14:22:27 -0400

    added SaveItem class and method

--- Assets/Scripts/For Battle/PlayerandEnemyActions.cs
+++ Assets/Scripts/For Battle/PlayerandEnemyActions.cs
@@ -26,6 +26,13 @@ public class PlayerandEnemyActions : MonoBehaviour
         Debug.Log(_playerUnit.characterStats.CharacterName + " " + _move.AttackName + ", on " + _allyTarget.characterStats.CharacterName);
     }
 
+    public void SaveItemUsage(Unit _playerUnit, ItemObject _item, Unit _theTarget)
+    {
+        playerActionContainer.Add(new SavePlayerActions(_playerUnit, _item, _theTarget));
+        Debug.Log(playerActionContainer.Count);
+        Debug.Log(_playerUnit.characterStats.CharacterName + " " + _item.ItemName + ", on " + _theTarget.characterStats.CharacterName);
+    }
+
     //saving enemy attack and defuffs
     public void SaveEnemyAndDebuffs(Unit _enemyUnit, MoveBaseClass _move, Unit _playerUnit)
     {
@@ -42,38 +49,37 @@ public class PlayerandEnemyActions : MonoBehaviour
         Debug.Log(_enemyUnit.characterStats.CharacterName + " " + _move.AttackName + ", on " + _alliedEnemyUnit.characterStats.CharacterName);
     }
 
+    public void SaveItems() { }
+
 
 
     public class SavePlayerActions
     {
         public MoveBaseClass move;
+        public ItemObject item;
         public Unit playerUnit;
-        public Unit enemyTarget;
-        public Unit allyTarget;
-
+        public Unit theTarget;
 
         //saving player attacks and debuffs
-        public SavePlayerActions(Unit _playerUnit, MoveBaseClass _move, Unit _enemyUnit)
+        public SavePlayerActions(Unit _playerUnit, MoveBaseClass _move, Unit _theTarget)
         {
             playerUnit = _playerUnit;
             move = _move;
-            enemyTarget = _enemyUnit;
+            theTarget = _theTarget;
         }
 
-/*        //saving player buffs and heals
-        public SavePlayerActions(Unit _playerUnit, MoveBaseClass _move, Unit _allyTarget)
+        public SavePlayerActions(Unit _playerUnit, ItemObject _item, Unit _theTarget)
         {
             playerUnit = _playerUnit;
-            move = _move;
-            allyTarget = _allyTarget;
-        }*/
+            item = _item;
+            theTarget = _theTarget;
+        }
     }
 
     public class SaveEnemyActions
     {
         public Unit enemyUnit;
         public Unit playerTarget;
-        public Unit alliedEnemyTarget;
         public MoveBaseClass move;
 
         //saving enemy attacks and debuffs
@@ -83,89 +89,5 @@ public class PlayerandEnemyActions : MonoBehaviour
             move = _move;
             playerTarget = _playerUnit;
         }
-
-        /*  //saving enemy buffs and heals
-          public SaveEnemyActions(Unit _enemyUnit, MoveBaseClass _move, Unit _alliedEnemyUnit)
-          {
-              enemyUnit = _enemyUnit;
-              move = _move;
-              alliedEnemyTarget = _alliedEnemyUnit;
-          }*/
     }
 }
-/*
- * public List<PlayerActions> playerActionContainer = new List<PlayerActions>();
-    public List<PlayerSave> playerSaveContainer = new List<PlayerSave>();
-    public List<EnemyActions> enemyActionContainer = new List<EnemyActions>();
-    public List<EnemySave> enemySaveContainer = new List<EnemySave>();
- * 
-    //creating a class to store each player's actions
-    public class PlayerActions
-    {
-        public MoveBaseClass move;
-        public Unit playerUnit;
-        public EnemyUnit enemyTarget;
-        Unit allyTarget;
-
-        public PlayerActions(Unit _playerUnit, MoveBaseClass _move, EnemyUnit _enemyUnit)
-        {
-            playerUnit = _playerUnit;
-            move = _move;
-            enemyTarget = _enemyUnit;
-        }
-
-        public PlayerActions(Unit _playerUnit, MoveBaseClass _move, Unit _allyTarget)
-        {
-            playerUnit = _playerUnit;
-            move = _move;
-            allyTarget = _allyTarget;
-        }
-
-        
-
-    }
-    
-    //creating a class to store each player's actions
-    public class PlayerSave
-    {
-        public MoveBaseClass move;
-        public Unit playerUnit;
-        public Unit allyTarget;
-
-        public PlayerSave(Unit _playerUnit, MoveBaseClass _move, Unit _allyTarget)
-        {
-            playerUnit = _playerUnit;
-            move = _move;
-            allyTarget = _allyTarget;
-        }
-
-    }
-    public class EnemyActions
-    {
-        public MoveBaseClass move;
-        public EnemyUnit enemyUnit;
-        public Unit playerTarget;
-
-
-        public EnemyActions(EnemyUnit _enemyUnit, MoveBaseClass _move, Unit _playerUnit)
-        {
-            enemyUnit = _enemyUnit;
-            move = _move;
-            playerTarget = _playerUnit;
-        }
-    }
-    
-    public class EnemySave
-    {
-        public MoveBaseClass move;
-        public EnemyUnit enemyUnit;
-        public EnemyUnit alliedEnemyTarget;
-
-
-        public EnemySave(EnemyUnit _enemyUnit, MoveBaseClass _move, EnemyUnit _alliedEnemyUnit)
-        {
-            enemyUnit = _enemyUnit;
-            move = _move;
-            alliedEnemyTarget = _alliedEnemyUnit;
-        }
-    }*/
